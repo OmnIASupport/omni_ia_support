8dfa45a7cca0a275796515bf25a205d443647cd1
llaokk <lipefortinite.9@gmail.com>
2026-09-25 10:49:59 -0300

    Add files via upload

---
+++ backeend/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+import os
+
+app = FastAPI()
+
+#configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Rota principal que abre o HTML
+@app.get("/")
+def home():
+    return FileResponse(INDEX_FILE)
---
+++ backeend/models.py
@@ -0,0 +1,3 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from database import Base
